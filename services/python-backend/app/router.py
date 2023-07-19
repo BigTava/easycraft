ed15bccc7dae6e13d9e8643740a4fbc702ff7a9b
Tiago Tavares <bigtavadev@gmail.com>
2023-07-19 14:13:33 +0100

    feat: runs in bacalhau

--- services/python-backend/app/router.py
+++ services/python-backend/app/router.py
@@ -20,10 +20,6 @@ async def post_order(
         # output = await run_in_bacalhau(payload.order)
         output = call_prompt(create_order_input(payload.order))
         generated_text = output[0]["generated_text"]
-        response = {}
-        response["message"] = generated_text
-        response["orderId"] = "0x9269195a0e6303c68fc632ed9e0b7dd83a4517946cfc94cc99b2e6b25f5d2447"
-        response["capacityId"] = "0xb15bf9f04e846c844a244f94b582e0634469d275a92ca234f65088a60166a449"
-        response["amount"] = 5
-        return {"message": response["message"], "orderId": response["orderId"], "capacityId": 
-                response["capacityId"], "amount": response["amount"]}
+
+        return {"message": generated_text["message"], "orderId": generated_text["orderId"], "capacityId": 
+                generated_text["capacityId"], "amount": generated_text["amount"]}
