b57cf658fd3152552a721e1aec20b7e2d461cde6
Tiago Tavares <bigtavadev@gmail.com>
2023-07-19 09:36:57 +0100

    refactor: marketplace

--- evm-contracts/hardhat.config.js
+++ evm-contracts/hardhat.config.js
@@ -1,19 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config()
+require("dotenv").config();
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
 require("./tasks/faucet");
 
-let accounts = { mnemonic: process.env.MNEMONIC }
+let accounts = { mnemonic: process.env.MNEMONIC };
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "gnosis",
   networks: {
-    hardhat: {
-    },
+    hardhat: {},
     gnosis: {
       url: "https://rpc.gnosischain.com",
       accounts: accounts,
